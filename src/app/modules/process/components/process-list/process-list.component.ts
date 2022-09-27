d433231a1290cb024a33bc7b347f231e9865dd60
Melissa <melihenaov@gmail.com>
2022-09-27 10:20:38 -0500

    Se continua editando vistas

--- src/app/modules/process/components/process-list/process-list.component.ts
+++ src/app/modules/process/components/process-list/process-list.component.ts
@@ -13,7 +13,9 @@ export class ProcessListComponent implements OnInit {
   ngOnInit(): void {
     let i=1;
     while(i < 12){
-      this.processes.push ({ title: 'titulo ' + i, description:'descripcion', id: i})
+      this.processes.push ({ title: 'titulo ' + i, description:`Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam voluptas ad unde impedit libero mollitia harum! Nisi eos sint velit pariatur magnam autem asperiores, enim facere exercitationem. Facere, consectetur doloremque.
+      Nobis nisi ducimus libero consequuntur laborum molestias dolorum. Animi optio minus incidunt maiores quaerat alias consequatur, cum doloribus asperiores sapiente. Harum provident voluptatem veritatis, maiores reiciendis laudantium eos magni eius.
+      Doloremque, facere molestiae? Fuga omnis voluptatem maxime incidunt quasi maiores numquam aperiam perspiciatis repudiandae nemo accusantium, hic fugit excepturi minus enim inventore magnam ab dolorem temporibus nesciunt vel suscipit. Facilis.` + i, id: i})
       i++ 
     }
   }
