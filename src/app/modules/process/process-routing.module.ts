9065a399eec71b2908b4e3c026647996beba7f4d
Melissa <melihenaov@gmail.com>
2022-09-28 09:51:49 -0500

    responsive vistas process

--- src/app/modules/process/process-routing.module.ts
+++ src/app/modules/process/process-routing.module.ts
@@ -3,6 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProcessLayoutComponent } from './components/process-layout/process-layout.component';
 import { ProcessListComponent } from './components/process-list/process-list.component';
 import { ProcessDetailsComponent } from './components/process-details/process-details.component';
+import { ProcessActivityDetailComponent } from './components/process-activity-detail/process-activity-detail.component';
+import { ProcessDocumentDetailComponent } from './components/process-document-detail/process-document-detail.component';
 
 const routes: Routes = [
   {
@@ -20,6 +22,14 @@ const routes: Routes = [
     {
       path:'process-details/:title',
       component:ProcessDetailsComponent
+    },
+    {
+      path:'process-activity-detail',
+      component:ProcessActivityDetailComponent
+    },
+    {
+      path:'process-document-detail',
+      component:ProcessDocumentDetailComponent
     }
 ]}
 ];
