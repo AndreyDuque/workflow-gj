8dd5ec075f60736b798fcd9e6fc3bd3cbde230ce
Andrey <andreyduque1987@gmail.com>
2022-09-30 13:58:05 -0500

    Se cargan datos a documentos internos

--- src/app/modules/process/components/process-document-detail/process-document-detail.component.ts
+++ src/app/modules/process/components/process-document-detail/process-document-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { B24Service } from 'src/app/modules/core/services/b24.service';
 
 @Component({
   selector: 'process-document-detail',
@@ -7,17 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProcessDocumentDetailComponent implements OnInit {
 
-  processes:any[]=[]
-  constructor() { }
+  processes: any[] = [];
+  documentDetalis: any = {};
+
+  constructor(
+    private readonly b24: B24Service
+  ) { }
 
   ngOnInit(): void {
-    let i=1;
-    while(i < 3){
-      this.processes.push ({ title: 'titulo ' + i, description:`Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam voluptas ad unde impedit libero mollitia harum! Nisi eos sint velit pariatur magnam autem asperiores, enim facere exercitationem. Facere, consectetur doloremque.
-      Nobis nisi ducimus libero consequuntur laborum molestias dolorum. Animi optio minus incidunt maiores quaerat alias consequatur, cum doloribus asperiores sapiente. Harum provident voluptatem veritatis, maiores reiciendis laudantium eos magni eius.
-      Doloremque, facere molestiae? Fuga omnis voluptatem maxime incidunt quasi maiores numquam aperiam perspiciatis repudiandae nemo accusantium, hic fugit excepturi minus enim inventore magnam ab dolorem temporibus nesciunt vel suscipit. Facilis.` + i, id: i})
-      i++ 
-    }
+
+    this.getDocumentDetails();
+    // let i=1;
+    // while(i < 3){
+    //   this.processes.push ({ title: 'titulo ' + i, description:`Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam voluptas ad unde impedit libero mollitia harum! Nisi eos sint velit pariatur magnam autem asperiores, enim facere exercitationem. Facere, consectetur doloremque.
+    //   Nobis nisi ducimus libero consequuntur laborum molestias dolorum. Animi optio minus incidunt maiores quaerat alias consequatur, cum doloribus asperiores sapiente. Harum provident voluptatem veritatis, maiores reiciendis laudantium eos magni eius.
+    //   Doloremque, facere molestiae? Fuga omnis voluptatem maxime incidunt quasi maiores numquam aperiam perspiciatis repudiandae nemo accusantium, hic fugit excepturi minus enim inventore magnam ab dolorem temporibus nesciunt vel suscipit. Facilis.` + i, id: i})
+    //   i++
+    // }
+  }
+
+  getDocumentDetails() {
+    this.b24.spaFieldList(146, 12).subscribe({
+      'next': (documentDetalis: any) => {
+        this.documentDetalis = documentDetalis.result.item;
+        console.log('Detalles del documento:', this.documentDetalis);
+      },
+      'error': documentDetalis => console.log(documentDetalis)
+    })
   }
 
 }
